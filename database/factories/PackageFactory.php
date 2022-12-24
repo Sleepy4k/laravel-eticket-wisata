57f653ed66398d95b400b2e7ba175c4df9145ad2
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-24 17:12:10 +0700

    feat: some factory
    add base feature factory

---
+++ database/factories/PackageFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Package;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Package>
+ */
+class PackageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Package::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->unique()->name(),
+            'description' => fake()->text(),
+            'price' => rand(10,50) . '000',
+            'tour_id' => rand(1,10)
+        ];
+    }
+}
---
+++ database/factories/TourFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tour;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tour>
+ */
+class TourFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Tour::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->unique()->name(),
+            'description' => fake()->text(),
+            'contact' => fake()->phoneNumber(),
+            'responsible' => rand(1,10),
+            'address' => fake()->address()
+        ];
+    }
+}
