5607e34f1a949b6339122ba37f80cabb1c63576a
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-24 17:13:16 +0700

    feat: some migration
    add base feature migration

---
+++ database/migrations/2022_12_24_064839_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_id')->unique();
+            $table->bigInteger('amount');
+            $table->string('total_price');
+            $table->date('payment_date');
+            $table->bigInteger('package_id');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
