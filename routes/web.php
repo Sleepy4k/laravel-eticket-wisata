9934a9b26d493d19bc38af8219bb3cfe9440dab4
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-27 09:48:30 +0700

    chore: route web
    add default admin endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Web\Auth;
+use App\Http\Controllers\Web\Admin;
+use App\Http\Controllers\Web\Audit;
+use App\Http\Controllers\Web\System;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -36,7 +40,7 @@ Route::get('/', fn() => view('welcome'))->name('landing.index');
 */
 
 Route::middleware('guest')->group(function() {
-    // Todo
+    Route::resource('login', Auth\LoginController::class, ['only' => ['index', 'store']]);
 });
 
 /*
@@ -50,5 +54,50 @@ Route::middleware('guest')->group(function() {
 */
 
 Route::middleware('auth')->group(function() {
-    // Todo
+    Route::post('logout', Auth\LogoutController::class)->name('logout.store');
+
+    // Dashboard Route
+    Route::prefix('dashboard')->as('dashboard.')->group(function() {
+        Route::get('/', Admin\DashboardController::class)->name('index');
+
+        // Admin Route
+        Route::prefix('admin')->as('admin.')->group(function() {
+            Route::resources([
+                'role' => Admin\RoleController::class,
+                'permission' => Admin\PermissionController::class
+            ]);
+        });
+
+        // System Route
+        Route::prefix('system')->as('system.')->group(function() {
+            Route::resource('application', System\ApplicationController::class, ['only' => ['index','create','store']]);
+            Route::resources([
+                'menu' => System\MenuController::class,
+                'page' => System\PageController::class,
+                'translate' => System\TranslateController::class
+            ]);
+        });
+
+        // Audit Route
+        Route::prefix('audit')->as('audit.')->group(function() {
+            Route::resources([
+                'auth' => Audit\AuthController::class,
+                'model' => Audit\ModelController::class,
+                'query' => Audit\QueryController::class,
+                'system' => Audit\SystemController::class
+            ], ['only' => ['index','show']]);
+        });
+    });
 });
+
+/*
+|--------------------------------------------------------------------------
+| Fallback Route
+|--------------------------------------------------------------------------
+| 
+| Please don't touch the code below unless you know what you're doing.
+| Also keep in mind to put this code at the bottom of the route for any route
+| listed below this code will not function or listed properly.
+*/
+
+Route::any('{any}', fn() => view('errors.404'))->where('any', '.*')->name('fallback');
