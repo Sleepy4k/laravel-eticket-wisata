30a2971de360d3caf7350fa166c3881fa357fb05
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-27 09:48:15 +0700

    chore: role seeder
    change default permission

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,7 +16,7 @@ class RoleSeeder extends Seeder
     {
         if (Role::count() == 0) {
             $roles = config()->get('role.seeder.list');
-            $permissions = config()->get('permission.seeder.role.user');
+            $permissions = config()->get('permission.seeder.role.loket');
 
             if (empty($roles)) {
                 throw new \Exception('Error: config/role.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.');
